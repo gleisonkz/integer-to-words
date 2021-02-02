f3915177d4a2086d89d70c0a9850562616a26ea1
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-02-02 00:08:59 -0300

    ✨ feat: test-reporter added

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'node',
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    ".+\\.ts$": "ts-jest",
+  },
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+      },
+    ],
+  ],
+};
