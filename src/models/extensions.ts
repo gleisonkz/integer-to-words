7d9e3300d3529f7c7bd92cb6f2dc5ad2965ec6cb
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-02-02 21:44:06 -0300

    ✨ feat: number extension functions

--- src/models/extensions.ts
+++ src/models/extensions.ts
@@ -3,3 +3,16 @@ interface Number {
   toArray: (type: StringConstructor | NumberConstructor) => (string | number)[];
   divisibleBy: (operand: number) => boolean;
 }
+
+Number.prototype.isBetween = function (min: number, max: number) {
+  return this >= min && this <= max;
+};
+
+Number.prototype.toArray = function (type: StringConstructor | NumberConstructor) {
+  const stringArray: string[] = [...this.toString()];
+  return stringArray.map((c) => type(c));
+};
+
+Number.prototype.divisibleBy = function (operand: number) {
+  return this % operand === 0;
+};
